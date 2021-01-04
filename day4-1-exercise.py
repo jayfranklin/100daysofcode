e7e6225aabf21cebc63fe1005af1307cef389eca
Jay Franklin <jayfranklin@users.noreply.github.com>
2021-01-04 13:45:05 -0500

    Create day4-1-exercise.py

---
+++ day4-1-exercise.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_float = random.random()
+#print(random_float)
+rounded_float = round(random_float)
+#print(rounded_float)
+if rounded_float == 1:
+  print("Heads")
+else:
+  print("Tails")
+
