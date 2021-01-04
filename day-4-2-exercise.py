2410be0094cc5499f282081f49829b593a5fe2de
Jay Franklin <jayfranklin@users.noreply.github.com>
2021-01-04 16:04:37 -0500

    Update day-4-2-exercise.py

--- day-4-2-exercise.py
+++ day-4-2-exercise.py
@@ -9,7 +9,7 @@ import random
 # Get length of list so we can feed that into the random integer function
 #   We could probably do this on the random.randint line, but this is probably easier to read
 length = len(names)
-random = random.randint(1,length)
+random = random.randint(0,length)
 
 #  We have to subtract 1 from our random integer because lists start at 0. 
 pick = str(names[random-1])
